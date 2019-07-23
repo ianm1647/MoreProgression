6e75e64ca23f760833e1eb24e18eb17703394cc9
ianm1647 <imcghee1647@gmail.com>
2019-07-23 13:44:26 -0500

    Delete RecipeTypes.java

--- src/main/java/com/moreprogression/main/tileentity/recipe/RecipeTypes.java
+++
@@ -1,9 +0,0 @@
-package com.moreprogression.main.tileentity.recipe;
-
-import net.minecraft.item.crafting.IRecipeType;
-
-public class RecipeTypes {
-
-	public static final IRecipeType<CrusherRecipe> CRUSHING = IRecipeType.register("moreprogression:crushing");
-	
-}
